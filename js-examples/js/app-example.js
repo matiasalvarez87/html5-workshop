fe9c2e956257b89708140f1e35618896f395753a
Matias Alvarez <matias.alvarez@globallogic.com>
2012-10-18 10:23:13 -0300

    Se agregan ejemplos JS y actualizacion de ejemplo socket.io

---
+++ js-examples/js/app-example.js
@@ -0,0 +1,3 @@
+function myMethod() {
+	$('#paragraph').html('Librerias cargadas correctamente, aca usamos jQuery');
+}
---
+++ js-examples/js/config.js
@@ -0,0 +1,22 @@
+/**
+ * config.js
+ * Author: Matias Alvarez
+ * Description: Contains the application configuration.
+ * */
+
+var Config = {
+
+	// TODO: Add all configurations in this file
+
+	dependencies: {
+
+		libs: [
+			"js/jquery-1.8.2.js",
+			"js/underscore.js"
+		],
+
+		application: [
+			"js/app-example.js"
+		]
+	}
+};
